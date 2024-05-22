3e140c1e4d63986c41de24f4e759beafdfca9888
mauricio.uehara <mauricio.uehara@piposaude.com.br>
2024-05-21 21:29:45 -0300

    Add supabase

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
-<script lang="ts">
-    import { Button } from "$lib/components/ui/button";
+<script>
+  export let data;
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<Button>Click me</Button>
+<ul>
+  {#each data.expenses as expense}
+    <li>{expense.Name}</li>
+  {/each}
+</ul>
