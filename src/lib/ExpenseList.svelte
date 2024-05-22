113fa9c9771e900ec274cb71e22b39ec07ef686d
mauricio.uehara <mauricio.uehara@piposaude.com.br>
2024-05-21 22:28:59 -0300

    Add expenses

---
+++ src/lib/ExpenseList.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let expense;
+</script>
+
+<li class="flex p-6 gap-6 max-w-md">
+  <div class="bg-zinc-200 h-12 w-12 rounded-full"></div>
+  <div class="grow">
+    <p class="font-medium">{expense['Name']}</p>
+    <p class="text-zinc-500 font-light">{expense['Paid By']}</p>
+  </div>
+  <p class="text-right">{expense['Amount']?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}</p>
+</li>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
 <script>
+  import ExpenseList from '$lib/ExpenseList.svelte';
   export let data;
 </script>
 
-<ul>
-  {#each data.expenses as expense}
-    <li>{expense.Name}</li>
-  {/each}
-</ul>
+<main class="flex flex-col items-center">
+  <header class="bg-indigo-500 w-full bg-zinc-800 p-10">
+    <h1 class="mt-10 scroll-m-20 text-white pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">Bom dia</h1>
+  </header>
+  <ul>
+    {#each data.expenses as expense}
+      <ExpenseList {expense} />
+    {/each}
+  </ul>
+</main>
