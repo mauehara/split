dfd76be08259ef8922c4f4ff055b8e8f58752793
mauricio.uehara <mauricio.uehara@piposaude.com.br>
2024-05-22 14:43:17 -0300

    Add expense date

--- src/lib/ExpenseList.svelte
+++ src/lib/ExpenseList.svelte
@@ -2,11 +2,17 @@
   export let expense;
 </script>
 
-<li class="flex p-6 gap-6 max-w-md">
-  <div class="bg-zinc-200 h-12 w-12 rounded-full"></div>
-  <div class="grow">
-    <p class="font-medium">{expense['Name']}</p>
-    <p class="text-zinc-500 font-light">{expense['Paid By']}</p>
+<li class="flex p-6 gap-5 max-w-md bg-white first:rounded-t-2xl last:rounded-b-2xl">
+  <div>
+    <p class="text-center text-sm text-zinc-500 font-light">{new Date(expense.created_at).getDate()}</p>
+    <p class="text-center text-sm text-zinc-500 font-light">{new Date(expense.created_at).toLocaleString("pt-BR", { month: "short" }).slice(0, -1)}</p>
   </div>
-  <p class="text-right">{expense['Amount']?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}</p>
+  <div class={"h-12 w-12 rounded-full flex justify-center items-center"} style={`background-color: ${expense.category.color}`}>
+    {expense.category.icon}
+  </div>
+  <div class="grow">
+    <p class="font-medium">{expense.name}</p>
+    <p class="text-sm text-zinc-500 font-light">{expense.paid_by.name}</p>
+  </div>
+  <p class="text-right">{expense.amount?.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}</p>
 </li>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,9 @@
   export let data;
 </script>
 
-<main class="flex flex-col items-center">
-  <header class="bg-indigo-500 w-full bg-zinc-800 p-10">
-    <h1 class="mt-10 scroll-m-20 text-white pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">Bom dia</h1>
+<main class="flex flex-col items-center bg-zinc-50">
+  <header class="w-full p-10">
+    <h1 class="mt-10 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">Olá</h1>
   </header>
   <ul>
     {#each data.expenses as expense}
