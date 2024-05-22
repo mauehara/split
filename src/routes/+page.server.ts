abe011c186d3fba567a3389ac55931cecc33e3c4
mauricio.uehara <mauricio.uehara@piposaude.com.br>
2024-05-22 16:02:15 -0300

    Fix expenses

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,8 @@
 import { supabase } from "$lib/supabaseClient";
 
 export async function load() {
-  const { data } = await supabase.from("expenses").select();
-  console.log(data);
+  const { data } = await supabase.from("expenses").select(`id, name, amount, created_at, paid_by ( name ), category ( name, icon, color)`);
+  
   return {
     expenses: data ?? [],
   };
