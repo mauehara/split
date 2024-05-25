d43abdd3ae8f823faf58373bb3fcfad11ee7fc77
mauricio.uehara <mauricio.uehara@piposaude.com.br>
2024-05-24 21:24:52 -0300

    Add allowed emails

---
+++ src/routes/signin/+error.svelte
@@ -0,0 +1,1 @@
+<p>Acesso proibido</p>
--- src/routes/signin/+page.server.ts
+++ src/routes/signin/+page.server.ts
@@ -1,9 +1,17 @@
 import type { PageServerLoad } from "./$types"
-import { redirect } from "@sveltejs/kit";
+import { redirect, error } from "@sveltejs/kit";
+
+const ALLOWED_EMAILS = [
+  "mau.uehara@gmail.com",
+  "liasegre@gmail.com"
+];
 
 export const load: PageServerLoad = async (events) => {
-  const session = await events.locals.auth()
-  console.log(session);
+  const session = await events.locals.auth();
+
+  if (session?.user?.email && !ALLOWED_EMAILS.includes(session.user.email)) {
+    error(403, {message: "You are not allowed to access this page"});
+  }
 
   if (session?.user?.name) {
     redirect(303, `/`)
