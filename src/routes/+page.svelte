199ea4ef5d916594b3d0d8ff2c82975d6eebd05f
mauricio.uehara <mauricio.uehara@piposaude.com.br>
2024-06-30 20:24:12 -0300

    refactor: Add greeting based on time of day in page component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,13 +6,23 @@
   export let data;
 
   $: balance = data.balance.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
+
+  const greeting = () => {
+    const date = new Date();
+    const hours = date.getHours();
+    if (hours >= 0 && hours < 12) return '🌤️ Bom dia';
+    if (hours >= 12 && hours < 18) return '☀️ Boa tarde';
+    return '🌒 Boa noite';
+  };
   
 </script>
 
 <Drawer allExpenses={data.stream.allExpenses} categories={data.stream.categories} userEmail={data.session.user?.email} />
 <main class="flex flex-col items-center bg-zinc-50" data-vaul-drawer-wrapper>
   <header class="w-full p-4 pb-0 flex justify-between">
-    <h1 class="mt-10 mb-0 scroll-m-20 text-md tracking-tight transition-colors first:mt-0">{`Olá, ${data.session.user?.name?.split(' ')[0]}`}</h1>
+    <h1 class="mt-10 mb-0 scroll-m-20 text-md tracking-tight transition-colors first:mt-0">
+      {`${greeting()}, ${data.session.user?.name?.split(' ')[0]}`}
+    </h1>
     <Avatar.Root>
       <Avatar.Image src={data.session.user?.image} alt="Foto de perfil" />
       <Avatar.Fallback>{data.session.user?.name?.split(' ').map(word => word[0]).join('')}</Avatar.Fallback>
